c8c199b71ec703e31d1b2aefcfd783295c6e351e
Unknown <morgan.p026@stu.nebo.edu>
2018-03-09 10:25:59 -0700

    boop

---
+++ DOM/colorchange.js
@@ -0,0 +1,9 @@
+//Variables//
+//This grabs the id of the first modal and sets it equal to the modal
+var modal = 
+    document.getElementById("modal");
+//This grabs the button with the id "change"
+//this allows us to call a function with a click later
+var changeBtn =
+    document.getElementById("modal");
+//This grabs 
